d102a318d55bdab9030f278b6e39c5c1a697f1f9
Paula <paisamares12@gmail.com>
2026-06-01 22:22:38 -0500

    Socorro

--- src/main/java/com/udmarketplace/auth/controller/AdminUserController.java
+++ src/main/java/com/udmarketplace/auth/controller/AdminUserController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
